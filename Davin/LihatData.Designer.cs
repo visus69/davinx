550649839463494ab3463738308393137a3cde4b
Akhdan Ferdiansyah Ramadhan <107316594+TnMarty2@users.noreply.github.com>
2023-01-08 18:59:12 +0700

    Add files via upload
    Commit Utama

---
+++ Davin/LihatData.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Davin
+{
+    partial class LihatData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgBarang = new System.Windows.Forms.DataGridView();
+            this.btnBack = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgBarang)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgBarang
+            // 
+            this.dgBarang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgBarang.Location = new System.Drawing.Point(16, 16);
+            this.dgBarang.Name = "dgBarang";
+            this.dgBarang.Size = new System.Drawing.Size(656, 472);
+            this.dgBarang.TabIndex = 0;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(696, 16);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(150, 40);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.Text = "BACK";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // LihatData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 511);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.dgBarang);
+            this.Name = "LihatData";
+            this.Text = "LihatData";
+            ((System.ComponentModel.ISupportInitialize)(this.dgBarang)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgBarang;
+        private System.Windows.Forms.Button btnBack;
+    }
+}
